2e9cd08876aa411157e01635fc81a6b13901fc14
stevecyj <ycsteven.tsao@gmail.com>
2023-12-22 19:23:48 +0800

    feat: refactor event handling and data display
    - Add a click event handler for the button element
    - Update the code to display the value of dataShow instead of data
    - Remove the commented out code for refreshing on error
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,25 @@
 <script setup lang="ts">
+// import { ref } from "vue";
 import Home from "@/components/Home.vue";
+let dataShow = ref<unknown>();
+
 const { data, error, refresh } = await useFetch(
   "https://od.moi.gov.tw/api/v1/rest/datastore/301000000A-002233-001",
   {
     server: true,
   }
 );
-// if (error) refresh();
 console.log("data =====> ", data);
+const handleClick = () => {
+  console.log("handleClick");
+  dataShow.value = data;
+};
 </script>
 
 <template>
   <div>Home</div>
-  <code>{{ data }}</code>
+  <code>{{ dataShow }}</code>
+  <button @click="handleClick">Click</button>
   <Home />
   <Box />
 </template>
