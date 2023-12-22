0c485d45ecefe958e000c900f63b2243ded90c18
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-12-22 16:46:30 +0800

    chore: update fetch URL and remove baseURL option from useFetch function call
    - Modify the fetch URL to "https://od.moi.gov.tw/api/v1/rest/datastore/301000000A-002233-001"
    - Remove the baseURL option from the useFetch function call
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Home from "@/components/Home.vue";
-const { data, error, refresh } = await useFetch("/VsWeb/api/GetLstDicCinema", {
-  baseURL: "https://www.vscinemas.com.tw",
-  server: true,
-});
+const { data, error, refresh } = await useFetch(
+  "https://od.moi.gov.tw/api/v1/rest/datastore/301000000A-002233-001",
+  {
+    server: true,
+  }
+);
 // if (error) refresh();
 console.log("data =====> ", data);
 </script>
