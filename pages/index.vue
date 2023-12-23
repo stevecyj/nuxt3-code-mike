b9436f7ec04a86a873b5216beefb4eaf83eb2e13
stevecyj <ycsteven.tsao@gmail.com>
2023-12-23 17:22:24 +0800

    refactor: remove unused code and add API data to template
    - Remove unused import statement
    - Remove commented out code
    - Remove console.log statement
    - Add API data to template
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,26 @@ import Home from "@/components/Home.vue";
 const { count, addCount } = useAddCount();
 const store = useHomeStore();
 const selectedDate = ref(new Date());
-/**
- * plugin
- * This code imports the `$hello` function from the `useNuxtApp` module.
- * The `$hello` function is used to perform some specific action in the Nuxt app.
- */
 const { $hello } = useNuxtApp();
+const { data } = await useFetch(
+  "https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0005-001?Authorization=rdec-key-123-45678-011121314",
+  {
+    server: true,
+    onRequest({ request, options }) {
+      // 設置 request headers
+      options.headers = options.headers || {};
+      options.headers = {
+        ...options.headers,
+        Authorization: "Bearer token",
+      };
+      console.log(options.headers);
+    },
+    onResponse({ request, response, options }) {
+      // 處理回傳資料
+      return response._data;
+    },
+  }
+);
 const getApiData = async () => {
   const res = await fetch("/api/hello").then((res) => res.json());
   console.log(res);
@@ -16,6 +30,7 @@ const getApiData = async () => {
 </script>
 
 <template>
+  {{ data }}
   <div>Home: {{ count }}</div>
   <button @click="addCount">home add count</button>
   <div>Store: {{ store.count }}</div>
