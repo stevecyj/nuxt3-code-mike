ae47745290a5cf302613546135e1aefe38857532
stevecyj <ycsteven.tsao@gmail.com>
2023-12-24 12:59:56 +0800

    feat: add reactive data fetching functionality
    - Add a new reactive object `dataShow` with an empty `data` property
    - Modify the `useFetch` function call to include a `refresh` function
    - Add a new function `getDataFromUseFetch` that calls the `refresh` function
    - Add a new button to the template that calls the `getDataFromUseFetch` function
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -4,25 +4,32 @@ const { count, addCount } = useAddCount();
 const store = useHomeStore();
 const selectedDate = ref(new Date());
 const { $hello } = useNuxtApp();
-const { data } = await useFetch(
+
+const dataShow = reactive({
+  data: "",
+});
+
+const { data, refresh } = await useFetch(
   "https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0005-001?Authorization=rdec-key-123-45678-011121314",
   {
     server: true,
-    // onRequest({ request, options }) {
-    //   // 設置 request headers
-    //   options.headers = options.headers || {};
-    //   options.headers = {
-    //     ...options.headers,
-    //     Authorization: "Bearer token",
-    //   };
-    //   console.log(options.headers);
-    // },
-    // onResponse({ request, response, options }) {
-    //   // 處理回傳資料
-    //   return response._data;
-    // },
+    onRequest({ request, options }) {
+      // 設置 request headers
+      // options.headers = options.headers || {};
+      options.headers = { authorization: "rdec-key-123-45678-011121314" };
+      console.log("options =====> ", options);
+    },
+    onResponse({ request, response, options }) {
+      // 處理回傳資料
+      return response._data;
+    },
   }
 );
+
+const getDataFromUseFetch = async () => {
+  await refresh();
+};
+
 const getApiData = async () => {
   const res = await fetch("/api/hello").then((res) => res.json());
   console.log(res);
@@ -31,6 +38,8 @@ const getApiData = async () => {
 
 <template>
   {{ data }}
+  <br />
+  <button @click="getDataFromUseFetch">get data from useFetch</button>
   <div>Home: {{ count }}</div>
   <button @click="addCount">home add count</button>
   <div>Store: {{ store.count }}</div>
